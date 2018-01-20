20e1b3bb94a5ca3f171b2ffa7d5e9aa77f28d1c1
Pat Newell <pnewell+git@renttherunway.com>
2018-01-20 14:01:01 -0500

    [Feature Request] Prioritize Primary Shop when multiple Shops match domain

--- server/api/core/core.js
+++ server/api/core/core.js
@@ -394,6 +394,9 @@ export default {
       limit: 1,
       fields: {
         _id: 1
+      },
+      orderby: {
+        merchantShops: -1 // give preference to PrimaryShop if multiple results
       }
     }).fetch()[0];
     return shop && shop._id;
